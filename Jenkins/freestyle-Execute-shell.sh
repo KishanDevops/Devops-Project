73ca572c283b3cf0e4d4c348573b3ff6b6fc70d0
KishanDevOps <lokhilkishan60@gmail.com>
2025-09-17 18:07:17 +0530

    add freestyle and declarative pipeline

---
+++ Jenkins/freestyle-Execute-shell.sh
@@ -0,0 +1,12 @@
+#Freestyle Projects Execute shell
+
+
+echo "Hello"
+
+mkdir -p devops
+
+echo "This is a Freestyle jenkins CD"
+
+# save and apply and run to play Button
+#check:- cd /var/lib/jenkins/workspace/<Project-name>/
+# ls
